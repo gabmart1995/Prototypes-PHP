e74b0d1431adf3ff0014734a48e76a0aab9dfa44
Gabriel Martínez <gabmart1995@gmail.com>
2019-08-15 15:43:21 -0400

    Funcionalidad basica del calendario

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,15 +1,56 @@
-document.addEventListener( 'DOMContentLoaded', () =>  {
+let calendarEl = document.getElementById( 'calendar' );
 
-	var calendarEl = document.getElementById( 'calendar' );
-	
-	var calendar = new FullCalendar.Calendar( calendarEl, {
-					
-		plugins: [ 'dayGrid', 'interaction' ],
-		
-		dateClick: ( info ) => {
-			console.log( `click el ${ info.dateStr }` );
+// botones personalizados
+let botones = {
+
+	mensaje: {
+		text: 'Mensaje',
+		click: () => {
+			alert( 'Hola mundo' );
 		}
-	});
+	},
+
+	accion: {
+		text: 'accion',
+		click: () => {
+			alert( 'Accion' );
+		}
+	}
+};
+
+let calendarOptions = {
+
+	plugins: [ 'dayGrid', 'interaction', 'moment', 'timeGrid' ],
+	locale: 'es',
+
+	// header
+	header: {
+		left: 'today,prev,next', // boton1,boton2',
+		center: 'title',
+		right: 'dayGridMonth,dayGridWeek,dayGridDay'
+	},
+
+	// botones personalizados
+	customButtons: {
+		boton1: botones.mensaje,
+		boton2: botones.accion
+	},
+
+	dateClick: ( response ) => {
+		
+		// console.log( response );
+
+		console.log( `Valor seleccionado: ${ response.dateStr }` );
+		console.log( `Vista actual: ${ response.view.type }` );
+
+		// let style = response.dayEl.style;
+		// style.backgroundColor = 'red';
+	}
+};
+
+document.addEventListener( 'DOMContentLoaded', () => {
+
+	const calendar = new FullCalendar.Calendar( calendarEl, calendarOptions );
 
 	calendar.render();
 });
--- calendarioWeb/index.php
+++ calendarioWeb/index.php
@@ -5,14 +5,29 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 		<link rel="stylesheet" href="./../assets/vendor/core/main.css">
 		<link rel="stylesheet" href="./../assets/vendor/daygrid/main.css">
-		<link rel="stylesheet" href="./../assets/calendar.css">
+		<link rel="stylesheet" href="./../assets/css/calendar.css">
+		<link rel="stylesheet" href="./../assets/vendor/bootstrap/bootstrap.css">
 		<script src="./../assets/vendor/core/main.js"></script>
 		<script src="./../assets/vendor/interaction/main.js"></script>
+		<script src="./../assets/vendor/moment/main.js"></script>
 		<script src="./../assets/vendor/daygrid/main.js"></script>
+		<script src="./../assets/vendor/timegrid/main.js"></script>
+		<script src="./../assets/js/es.js"></script>
 		<title>Prueba Fullcalendar</title>
 	</head>
 	<body>
-		<div id="calendar"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-12 mt-3 mb-4">
+					<div id="calendar"></div>
+				</div>
+			</div>
+		</div>
+		<!-- =============================== -->
+		<!-- 		JS 					 -->
+		<!-- =============================== -->
+		<script src="./../assets/vendor/jquery/jquery-3.4.1.min.js"></script>
+		<script src="./../assets/vendor/bootstrap/bootstrap.js"></script>
 		<script src="./../assets/js/calendar.js"></script>
 	</body>
-</html> 
+</html>
