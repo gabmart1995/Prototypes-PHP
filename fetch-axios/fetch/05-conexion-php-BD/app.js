79b82996f984783d17ef72072931f2368e5539d2
gabmart1995 <gabmart1995@gmail.com>
2020-03-22 12:24:46 -0400

    ajustes de estilo css a la vista 05 w3.css

--- fetch-axios/fetch/05-conexion-php-BD/app.js
+++ fetch-axios/fetch/05-conexion-php-BD/app.js
@@ -1,6 +1,7 @@
 const formulario = document.getElementById('formulario');
 const insert = document.getElementById('respuesta-insert');
 const table = document.getElementById('table');
+const formEdit = document.getElementById('editForm');
 
 formulario.addEventListener('submit', function( $event ) {
 
@@ -37,7 +38,7 @@ function sendData( request ) {
 
 				console.log( data );
 				
-				showMessage( 'El usuario ha sido creado con éxito' );
+				showMessage( 'El usuario ha sido creado con éxito', 'w3-text-green' );
 			}
 
 		})
@@ -73,10 +74,10 @@ function consultAllGuest() {
 		});
 }
 
-function showMessage( message ) {
+function showMessage( message, className ) {
 
 	insert.innerHTML = `
-		<p>${ message }</p>
+		<p class=${ className }>${ message }</p>
 	`
 }
 
@@ -97,8 +98,24 @@ function showTable( guestArray ) {
 					<td>${ guest.lastname }</td>
 					<td>${ guest.email }</td>
 					<td>${ guest.reg_date }</td>
+					<td>
+						<button onclick="editGuest(
+								{ 
+									id: ${ guest.id }, 
+									firstname: '${ guest.firstname }',
+									lastname: '${ guest.lastname }',
+									email: '${ guest.email }',
+									reg_date: '${ guest.reg_date }'
+								})" class="w3-btn w3-blue">
+							Editar
+						</button>
+						<button onclick="deleteGuest( ${ guest.id } )" class="w3-btn w3-red">
+							Eliminar
+						</button>
+					</td>
+					<td></td>
 				</tr>
-			`
+			`;
 		});
 
 		message.hidden = true;
@@ -108,4 +125,25 @@ function showTable( guestArray ) {
 		message.hidden = false;
 	}
 
+}
+
+function editGuest( guest ) {
+
+	formEdit[0].value = guest.firstname;
+	formEdit[1].value = guest.lastname;
+	formEdit[2].value = guest.email;
+
+	document.getElementById('editModal').style.display = 'block';	
+}
+
+function deleteGuest( id ) {
+	
+	const title = document.getElementById('modal-title');
+	title.innerText = 'Eliminar usuario ' + id;
+
+	document.getElementById('deleteModal').style.display = 'block'
+}
+
+function closeModal( idElement ) {
+	document.getElementById( idElement ).style.display = 'none';
 }
